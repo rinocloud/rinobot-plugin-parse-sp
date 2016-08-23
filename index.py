a7b99c4d4e22392a3e196023a8011613ce6d36fa
eoinmurray <amonecho1@gmail.com>
2016-08-23 13:17:47 +0200

    python3 working, parsing .sp

---
+++ index.py
@@ -0,0 +1,124 @@
+from numpy import *
+import sys
+
+DSet2DC1DIBlock = 120
+DataSetAbscissaRangeMember = -29838
+DataSetIntervalMember = -29836
+DataSetNumPointsMember = -29835
+DataSetXAxisLabelMember = -29833
+DataSetYAxisLabelMember = -29832
+DataSetDataMember = -29828
+DataSetNameMember = -29827
+DataSetAliasMember = -29823
+
+def eof(fd):
+    last_pos = fd.tell()
+
+    if fd.read(1) == "":
+        return True
+    elif len(fromfile(fd, int16, 1)) == 0:
+        return True
+    else:
+        fd.seek(last_pos)
+        return False
+
+
+def loadfile(filename, verbose=False):
+    fd = open(filename, "rb")
+    fname = filename.split('/')[-1]
+    print("Converting %s" % fname)
+
+    signature = fromfile(fd, dtype="<i1", count=4)
+    signature = ''.join(map(chr, list(signature)))
+
+    if signature != "PEPE":
+        raise ValueError('Not valid Perkin Elmer file: %s' % signature)
+        return False, 0, 0, 0
+
+    try:
+        description = fromfile(fd, dtype="<i1", count=40)
+        description = ''.join(map(chr, list(description)))
+
+    except ValueError:
+        pass
+        description = ""
+
+    if verbose:
+        print(signature)
+        print(description)
+
+    xLen = int(0)
+
+    while not eof(fd):
+        blockID = fromfile(fd, int16, 1)[0]
+        blockSize = fromfile(fd, int32, 1)[0]
+
+        if blockID == DSet2DC1DIBlock:
+            pass
+
+        elif blockID == DataSetAbscissaRangeMember:
+            fromfile(fd, int16, 1)
+            x0 = fromfile(fd, float64, 1)
+            xEnd = fromfile(fd, float64, 1)
+
+        elif blockID == DataSetIntervalMember:
+            fromfile(fd, int16, 1)
+            xDelta = fromfile(fd, float64, 1)
+
+        elif blockID == DataSetNumPointsMember:
+            fromfile(fd, int16, 1)
+            xLen = fromfile(fd, int32, 1)
+
+        elif blockID == DataSetXAxisLabelMember:
+            fromfile(fd, int16, 1)
+            length = fromfile(fd, int16, 1)
+            xLabel = fromfile(fd, "<i1", length)
+            xLabel = ''.join(map(chr, list(xLabel)))
+
+        elif blockID == DataSetYAxisLabelMember:
+            fromfile(fd, int16, 1)
+            length = fromfile(fd, int16, 1)
+            yLabel = fromfile(fd, "<i1", length)
+            yLabel = ''.join(map(chr, list(yLabel)))
+
+        elif blockID == DataSetAliasMember:
+            fromfile(fd, int16, 1)
+            length = fromfile(fd, int16, 1)
+            alias = fromfile(fd, "<i1", length)
+            alias = ''.join(map(chr, list(alias)))
+
+        elif blockID == DataSetNameMember:
+            fromfile(fd, int16, 1)
+            length = fromfile(fd, int16, 1)
+            originalName = fromfile(fd, "<i1", length)
+            originalName = ''.join(map(chr, list(originalName)))
+
+        elif blockID == DataSetDataMember:
+            fromfile(fd, int16, 1)
+            length = fromfile(fd, int32, 1)
+            if xLen == 0:
+                xLen = length / 8
+            yData = fromfile(fd, float64, xLen)
+
+        else:
+            fd.seek(blockSize, 1)
+
+    xData = arange(x0, xEnd + xDelta, xDelta)
+
+    return xData, yData, xLabel, yLabel
+
+
+def convertFile(filename, verbose=False):
+    x, y, xL, yL = loadfile(filename, verbose)
+
+    if x is not False:
+        fname = filename.split('.')[0]
+        savetxt(fname + '.txt', vstack((x, y)).T, fmt="%1.6lf")
+        return True
+
+def main():
+    filename = sys.argv[1]
+    convertFile(filename)
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# rinobot-parse-sp
+
+Parses .sp files for the spectral data, does not parse out metadata.
